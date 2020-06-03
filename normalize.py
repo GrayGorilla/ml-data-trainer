a1c8b478e135c819f1634ce35b2904586b9c2e62
Nathan D. Brennan <nbren004@ucr.edu>
2020-06-03 06:53:33 -0700

    Main module can obtain raw data and normalize

--- normalize.py
+++ normalize.py
@@ -1,2 +1,6 @@
-def normalize(rawData):
-    return rawData
+def normalize(data):
+    cols = list(data.columns)
+    cols.remove(0)
+    
+    for col in cols:
+        data[col] = (data[col] - data[col].mean()) / data[col].std()
