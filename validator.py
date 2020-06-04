69075dae32e1d4a24ada5e70a24ca4cf75a84c0c
Nathan D. Brennan <nbren004@ucr.edu>
2020-06-04 07:05:19 -0700

    Implemented Leave-One-Out validator

--- validator.py
+++ validator.py
@@ -1,2 +1,13 @@
+from classifier import nearestNeighbor as classifier
+
 def leaveOneOutVal(dataFrame, features):
-    return 1
+    classes = dataFrame[0]
+    selectData = dataFrame[features]
+    score = 0
+    # Test the classifier: One test data point vs everything else (training data)
+    for testDataPt in range(0, len(selectData)):
+        guess = classifier(selectData, classes, testDataPt)
+        if guess == classes[testDataPt]:
+            score += 1
+
+    return score / len(selectData)
